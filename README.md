9835f2df30a4980e5de8acc7123dca00863e4c0e
nonchang <n_o_n_t_a_2_0_1_4@yahoo.co.jp>
2018-03-30 00:48:17 +0900

    Tween関数テスト中

--- README.md
+++ README.md
@@ -17,3 +17,53 @@
 
 
 `EOF`
+
+
+
+
+### 古いメモ
+
+
+- vanilla typescriptでどのようなui設計が可能か少し調べてみる。
+- flexboxの動的生成で制御可能かどうか。
+
+- まずcanvasが配置され、基本的なUIはその上にHTMLとして重ねるイメージ。
+	- 画面最上部・最下部などは広告枠などが要求されるかも。
+	- また、canvasのサイズを調整したい。
+
+
+- 制限事項
+	- jsだけでできないことは素直にcssを定義する。
+		- とりあえずリセットとして以下をhtml側に定義。ボタンフォーカスのアウトラインも邪魔。。
+			<style type="text/css">
+			*{
+				margin : 0 ; padding : 0 ; box-sizing: border-box ;
+			}
+			*:focus { outline: 0 }
+			</style>
+
+- メモ
+	- https://coliss.com/articles/build-websites/operation/css/css3-flexbox-properties-by-scotch.html
+		- flexboxで概ね設定できそうな気配。uGUIで言う所のhorizontal,vertical,grid layoutに相当する動きは問題なくいけそう。
+		- もちろんUnityと違って全体の比率をGUIで調節できる訳ではないので、計画ならびに「複雑すぎないUI」の検討が重要になると思うけど。
+
+- 実現したい挙動の案
+
+	- 下部ボタンを最低限のcssによる表現で。
+			- 9sliceに当たる表現は可能か？ →border-imageでいけそう。素材探しが難点かも。自作する？
+
+		- webフォントでも使えば十分雰囲気出せたりしないか？
+			https://liginc.co.jp/web/design/font/15639
+			- Googleフォントはすぐ使えるけど……無料で雰囲気のある和文フォントを探すのは辛いだろうなぁ。
+			- 欧文フォントは色々あるので、雰囲気作りのために欧文フォントの部分採用を検討するのは良いと思う。
+
+
+
+	- 下部ボタンの表示・非表示をtweenで表現したい
+			- 階層メニューをalpha含むtweenアニメーション付きで入れ替えたい。css animationかな？
+			- ここで、:hoverなど擬似要素はjavascriptから設定できないことを知る……。これ、「DOM要素じゃない」という説明を見かけるけどどういうことなんだろうなぁ。レンダリング結果に反映される要因なのだからAPI用意して欲しいところだけど＞＜；
+			- ということで、jsからサクッと使えるアニメーションライブラリを探す方法に切り替えた方が良いだろう。
+				- とりあえず見つかったanime.jsを試してみようか？
+				- TypeScriptソースごと晒してるようなライブラリがあると助かるのだけども。自作しちまおうかなぁ。
+
+`EOF`
--- src/main.ts
+++ src/main.ts
@@ -29,9 +29,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
 class Main{
 
-	//canvas
-	readonly CellDrawSize = 8
-
 
 	constructor(body: HTMLBodyElement){
 		//UIモジュールのテスト
@@ -48,19 +45,118 @@ class Main{
 
 		const ui = new Styler("div").fullWindow().appendTo(body).getElement()
 
-		console.log(ui)
+		// console.log(ui)
 	
-		const button = new Styler("button")
-			.bg(255,255,255,0.7)
-			.text("testButton")
-			.size(100,100)
-			.abs().r(0).b(0)
-			.appendTo(ui)
+
+		//右下ボタンテスト
+		// const button = new Styler("button")
+		// 	.bg(255,255,255,0.7)
+		// 	.text("testButton")
+		// 	.size(100,100)
+		// 	.abs().r(0).b(0)
+		// 	.appendTo(ui)
+		// 	.getElement()
+
+		// button.onclick = ()=>{ console.log("testtest") }
+
+
+		// button shorthand
+		var btn = (text: string, elm: HTMLElement) => {
+			var newButton = new Styler("button")
+				.text(text)
+				.size(100, 50)
+				.glow()
+				// .round(10)
+				.framed()
+				// .margin(3)
+				.appendTo(elm)
+				.getElement()
+				;
+			newButton.onmouseover = () => {
+				// console.log("onmouseover");
+				newButton.style.backgroundColor = "rgba(255,0,0,1)"
+				newButton.style.borderWidth = "10"
+				newButton.style.borderColor = "rgba(255,255,255,0.3)"
+			}
+			newButton.onmousedown = () => {
+				// console.log("onmouseover");
+				newButton.style.backgroundColor = "rgba(255,0,255,1)"
+			}
+			newButton.onmouseup = () => {
+				// console.log("onmouseover");
+				newButton.style.backgroundColor = "rgba(255,255,255,1)"
+			}
+			newButton.onmouseout = () => {
+				// console.log("onmouseout");
+				newButton.style.backgroundColor = "rgba(255,255,255,1)"
+				newButton.style.borderColor = "#fff"
+			}
+			newButton.onclick = (e) => {
+				e.preventDefault()
+				console.log("onclick");
+			}
+		}
+
+
+		// 下部ボタンをflex layoutで。
+
+		const bottomButtonLayout = new Styler("div")
+			.abs().fullWindow()//b()
+			.flexVertical()
+			.appendTo(body)
 			.getElement()
+			;
+		bottomButtonLayout.style.width = "100%";
 
-		button.onclick = ()=>{ console.log("testtest") }
+		// 下部ボタン以外「全て」
+		const mainContent = new Styler("div")
+			.appendTo(bottomButtonLayout)
+			.text("mainContents")
+			.getElement()
+		;
+		mainContent.style.display = "flex";
+		mainContent.style.flex = "1";
+		mainContent.style.border="solid"
+		mainContent.style.borderWidth="40px 40px 40px 40px"
+		mainContent.style.borderImage="url(borderimage_sample.png) 40 40 40 40 fill repeat"
+
+		//1列目
+		const bottomButtonLayoutSub1 = new Styler("div")
+			.height(50)
+			.flexHorizontal()
+			.appendTo(bottomButtonLayout)
+			.getElement()
+			;
+		bottomButtonLayoutSub1.style.width = "100%";
+
+		btn("atk", bottomButtonLayoutSub1)
+		btn("mgk", bottomButtonLayoutSub1)
+		btn("テスト", bottomButtonLayoutSub1)
+
+		//2列目
+		const bottomButtonLayoutSub2 = new Styler("div")
+			.height(50)
+			.flexHorizontal()
+			.appendTo(bottomButtonLayout)
+			.getElement()
+			;
+		bottomButtonLayoutSub2.style.width = "100%";
+
+		btn("4", bottomButtonLayoutSub2)
+		btn("5", bottomButtonLayoutSub2)
+		btn("6", bottomButtonLayoutSub2)
 
 
+
+		//ダイアログ - うまくいってない
+		// const div = new Styler("div")
+		// 	.text("123123")
+		// 	.marginWindow(80)
+		// 	.appendTo(body)
+		// 	.getElement()
+		// div.style.border="1px solid gray"
+		// div.style.borderWidth="40px 40px 40px 40px"
+		// div.style.borderImage="url(borderimage_sample.png) 40 40 40 40 fill repeat"
 
 
 
@@ -90,99 +186,170 @@ class Main{
 }
 
 
+
+
+
+
 //==================================================
 // Styler
 // 検討中: 
 // - jQueryのようにメソッドチェーンするCSS設定ユーティリティにできると嬉しい。
+//	- 面白がってjQueryの再発明をし続けないように注意。目的はHTML5ゲーム設計時のDOM生成とCSS周りの補佐。
+// - せっかくのTypeScriptなので、document.createElementの返す型を維持するように設計。
 // - CSSは「目的」に対して「決まった記述のセット」が多いように感じたため作成。
+// - flexboxによるUI検討のヘルパーにしていきたい。
 
-// 利用例
-// - `const canvas = new Styler("canvas").fullWindow().appendTo(body).getElement()`
+// 利用例:
+// - `const canvas = new Styler("canvas").fullWindow().appendTo(body).getElement()` //→HTMLCanvasElement型
 
 class Styler<T extends keyof HTMLElementTagNameMap>{
 
 	private elm: HTMLElementTagNameMap[T]
 
-	constructor(tagName: T){
+	constructor(tagName: T) {
 		this.elm = document.createElement(tagName)
 		return this
 	}
 
 	// set style shorthand
 
-	fullWindow(): Styler<T>{
-		this.elm.style.position = "absolute"
-		this.elm.style.width = "100%"
-		this.elm.style.height = "100%"
+	fullWindow(): Styler<T> {
+		// this.marginWindow(100);
+		this.abs()
+		this.elm.style.width = `100%`
+		this.elm.style.height = `100%`
 		return this
 	}
 
-	bg(r:number, g:number, b:number, a:number): Styler<T>{
+	// marginWindow(percent: number): Styler<T> {
+	// 	this.abs()
+	// 	this.elm.style.width = `{percent}%`
+	// 	this.elm.style.height = `{percent}%`
+	// 	return this
+	// }
+
+	bg(r: number | string, g?: number, b?: number, a?: number): Styler<T> {
+		if (typeof r === "string") {
+			this.elm.style.background = r
+			return this
+		}
 		this.elm.style.background = `rgba(${r},${g},${b},${a})`
 		return this
 	}
 
-	bordered1(): Styler<T>{
+	// height(num: number): Styler<T>{
+	// 	this.elm.style.height = "1px solid gray;"
+	// 	return this
+	// }
+
+	//UNDONE: 検討中
+	bordered1(): Styler<T> {
 		this.elm.style.border = "1px solid gray;"
 		return this
 	}
 
-	round(radius: number): Styler<T>{
+	margin(num: number = 1): Styler<T> {
+		this.elm.style.margin = `${num}px`
+		return this
+	}
+
+	padding(num: number = 1): Styler<T> {
+		this.elm.style.padding = `${num}px`
+		return this
+	}
+
+	round(radius: number): Styler<T> {
 		this.elm.style.borderRadius = `${radius}px`
 		return this
 	}
 
-	text(text: string): Styler<T>{
+	text(text: string): Styler<T> {
 		this.elm.innerText = text
 		return this
 	}
 
-	abs(): Styler<T>{
+	abs(): Styler<T> {
 		this.elm.style.position = "absolute"
 		return this
 	}
 
-	size(width: number, height: number): Styler<T>{
-		this.elm.style.width = `${width}px`
+	size(width: number | string, height: number | string): Styler<T> {
+		if (typeof width === "string") {
+			this.elm.style.width = width
+		} else {
+			this.elm.style.width = `${width}px`
+		}
+		if (typeof height === "string") {
+			this.elm.style.height = height
+		} else {
+			this.elm.style.height = `${height}px`
+		}
+		return this
+	}
+	height(height: number): Styler<T>{
 		this.elm.style.height = `${height}px`
+		return this
+	}
+	width(width: number): Styler<T>{
+		this.elm.style.width = `${width}px`
 		return this
 	}
 
 	//right, left, top, bottom
 
-	r(num: number): Styler<T>{
+	r(num: number = 0): Styler<T> {
 		this.elm.style.right = `${num}px`
 		return this
 	}
 
-	l(num: number): Styler<T>{
+	l(num: number = 0): Styler<T> {
 		this.elm.style.left = `${num}px`
 		return this
 	}
 
-	t(num: number): Styler<T>{
+	t(num: number = 0): Styler<T> {
 		this.elm.style.top = `${num}px`
 		return this
 	}
 
-	b(num: number): Styler<T>{
+	b(num: number = 0): Styler<T> {
 		this.elm.style.bottom = `${num}px`
+		return this
+	}
+
+	//flexbox
+
+	flexHorizontal(): Styler<T> {
+		this.elm.style.display = "flex"
+		this.elm.style.flexDirection = "row"
+		return this
+	}
+	flexVertical(): Styler<T> {
+		this.elm.style.display = "flex"
+		this.elm.style.flexDirection = "column"
+		return this
+	}
+	glow(num: number = 1): Styler<T> {
+		this.elm.style.flexGrow = `${num}`
+		return this
+	}
+
+	// framed image
+	framed(): Styler<T> {
+		this.elm.style.borderStyle = "solid"
+		this.elm.style.borderWidth = "16px 16px 16px 16px"
+		this.elm.style.borderImage = "url(frame-small.png) 16 16 16 16 fill repeat"
 		return this
 	}
 
 	// output utils
 
-	appendTo(dom: HTMLElement): Styler<T>{
+	appendTo(dom: HTMLElement): Styler<T> {
 		dom.appendChild(this.elm)
 		return this
 	}
 
-	// appendToStyler(styler: any): Styler<T>{
-	// 	styler.appendTo(this.elm)
-	// 	return this
-	// }
-
-	getElement(): HTMLElementTagNameMap[T]{
+	getElement(): HTMLElementTagNameMap[T] {
 		return this.elm
 	}
 
